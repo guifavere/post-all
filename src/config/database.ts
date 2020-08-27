911b13d764fd588c5a70f64f2231f0f0a1d461bc
guifavere <guilherme.favere@gmail.com>
2020-08-26 23:13:38 -0300

    Fix database config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,9 +1,12 @@
 import { ConnectionOptions } from 'typeorm';
 
+import Tag from 'entities/Tag';
+
 export default <ConnectionOptions>{
   type: 'mongodb',
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 27017,
-  name: process.env.DB_NAME,
+  database: process.env.DB_NAME,
   useUnifiedTopology: true,
+  entities: [Tag],
 };
