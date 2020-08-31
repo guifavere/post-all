96363ac368ab5a23039c8121acc4155319048c78
guifavere <guilherme.favere@gmail.com>
2020-08-31 20:13:56 -0300

    Create api route that displays tags

--- src/pages/api/tags.ts
+++ src/pages/api/tags.ts
@@ -3,13 +3,32 @@ import { NextApiResponse, NextApiRequest } from 'next';
 import connectDatabase from 'middlewares/connectDatabase';
 import Tag from 'entities/Tag';
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function create(req: NextApiRequest, res: NextApiResponse) {
   const { name } = req.body;
   const tag = new Tag(name);
 
   await req.db.save(tag);
 
   res.status(200).json([]);
+}
+
+async function index(req: NextApiRequest, res: NextApiResponse) {
+  const tags = await req.db.find('Tag');
+
+  res.status(200).json(tags);
+}
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case 'POST':
+      await create(req, res);
+      break;
+    case 'GET':
+      await index(req, res);
+      break;
+    default:
+      res.status(400).json({ message: 'Invalid method' });
+  }
 }
 
 export default connectDatabase(handler);
