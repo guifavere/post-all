4d97ae6c3eb83667ce7bf7343e660d9c2985c147
guifavere <guilherme.favere@gmail.com>
2020-08-31 20:33:33 -0300

    Create api route that update tag

--- src/pages/api/tags/[id].ts
+++ src/pages/api/tags/[id].ts
@@ -10,10 +10,22 @@ async function destroy(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json([]);
 }
 
+async function update(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query;
+  const { name } = req.body;
+
+  await req.db.update('Tag', { id }, { name });
+
+  res.status(200).json([]);
+}
+
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
     case 'DELETE':
       await destroy(req, res);
+      break;
+    case 'PUT':
+      await update(req, res);
       break;
     default:
       res.status(400).json({ message: 'Invalid method' });
