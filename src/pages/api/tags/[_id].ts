63f971acf35b50cbf6605e404db259e1bfc305d8
guifavere <guilherme.favere@gmail.com>
2020-10-05 18:36:58 -0300

    Add validation for api routes that destroy, and update tag

--- src/pages/api/tags/[_id].ts
+++ src/pages/api/tags/[_id].ts
@@ -1,37 +1,55 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { EntityManager } from 'typeorm';
 import { ObjectId } from 'mongodb';
+import * as yup from 'yup';
 
 import connectDatabase from 'pages/_middlewares/connectDatabase';
+import Tag from 'entities/Tag';
 
 interface ApiRequest extends NextApiRequest {
   db: EntityManager;
   query: {
     _id: string;
   };
+  tag: Tag;
 }
 
-async function destroy(req: ApiRequest, res: NextApiResponse) {
+async function validateDestroy(req: ApiRequest, res: NextApiResponse) {
   const { _id } = req.query;
-  const tag = await req.db.findOne('Tag', _id);
+  const tag: Tag = await req.db.findOne('Tag', _id);
 
-  if (!tag) return res.status(404).json({ message: 'Tag not found' });
+  if (!tag) {
+    res.status(404).json({ message: 'Tag not found' });
+  }
 
-  await req.db.delete('Tag', tag);
-
-  return res.status(204).json([]);
+  req.tag = tag;
 }
 
-async function update(req: ApiRequest, res: NextApiResponse) {
-  const { _id } = req.query;
+async function validateUpdate(req: ApiRequest, res: NextApiResponse) {
+  const schema = yup.object().shape({ name: yup.string().min(3).required() });
   const { name } = req.body;
 
-  if (!name) return res.status(422).json({ message: 'Invalid name' });
+  // validate body
+  try {
+    schema.validateSync({ name }, { abortEarly: false });
+  } catch (error) {
+    const message = 'Validation error';
+    const errors = error.inner.map((e: yup.ValidationError) => ({
+      [e.path]: e.message,
+    }));
 
-  const tag = await req.db.findOne('Tag', _id);
+    res.status(422).json({ message, errors });
+  }
 
-  if (!tag) return res.status(404).json({ message: 'Tag not found' });
+  // check tag exists
+  const { _id } = req.query;
+  const tag: Tag = await req.db.findOne('Tag', _id);
 
+  if (!tag) {
+    res.status(404).json({ message: 'Tag not found' });
+  }
+
+  // check there are tags same name
   const [, tagsSameNameLength] = await req.db.findAndCount('Tag', {
     where: {
       _id: { $not: { $eq: new ObjectId(_id) } },
@@ -39,20 +57,39 @@ async function update(req: ApiRequest, res: NextApiResponse) {
     },
   });
 
-  if (tagsSameNameLength)
-    return res.status(422).json({ message: 'Already exist tag same name' });
+  if (tagsSameNameLength) {
+    res.status(422).json({ message: 'Already exist tag same name' });
+  }
+
+  req.tag = tag;
+}
+
+async function destroy(req: ApiRequest, res: NextApiResponse) {
+  const { tag } = req;
+
+  await req.db.delete('Tag', tag);
+
+  res.status(204).json([]);
+}
+
+async function update(req: ApiRequest, res: NextApiResponse) {
+  const { tag } = req;
+  const { _id } = req.query;
+  const { name } = req.body;
 
   await req.db.update('Tag', tag, { name });
 
-  return res.status(200).json({ tag: { _id, name } });
+  res.status(200).json({ tag: { _id, name } });
 }
 
 async function handler(req: ApiRequest, res: NextApiResponse) {
   switch (req.method) {
     case 'DELETE':
+      await validateDestroy(req, res);
       await destroy(req, res);
       break;
     case 'PUT':
+      await validateUpdate(req, res);
       await update(req, res);
       break;
     default:
